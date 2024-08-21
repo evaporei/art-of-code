26ed3c83471ede8c2b4015460b73d556a773b720
Eva Pace <eba.pachi@gmail.com>
2024-08-21 17:32:03 -0300

    menger-sponge: make box struct

--- menger-sponge/src/main.zig
+++ menger-sponge/src/main.zig
@@ -1,5 +1,28 @@
 const rl = @import("raylib");
 const Vector3 = rl.Vector3;
+const Mesh = rl.Mesh;
+const Model = rl.Model;
+
+const Box = struct {
+    pos: Vector3,
+    model: Model,
+
+    fn init(x: f32, y: f32, z: f32) Box {
+        const pos = Vector3.init(x, y, z);
+        const mesh = rl.genMeshCube(200, 200, 200);
+        const model = rl.Model.fromMesh(mesh);
+        return Box{ .pos = pos, .model = model };
+    }
+
+    fn rotate(self: *Box) void {
+        self.model.transform = self.model.transform.multiply(rl.Matrix.rotateY(1.0 * 3.14159265358979323846 / 180.0));
+        self.model.transform = self.model.transform.multiply(rl.Matrix.rotateZ(1.0 * 3.14159265358979323846 / 180.0));
+    }
+
+    fn draw(self: *Box) void {
+        self.model.drawEx(self.pos, Vector3.init(1, 1, 1), 0.0, rl.Vector3.init(1, 1, 1), rl.Color.gold);
+    }
+};
 
 pub fn main() anyerror!void {
     const screenWidth = 800;
@@ -19,15 +42,12 @@ pub fn main() anyerror!void {
     rl.disableCursor(); // limit cursor to relative moment inside window
     rl.setTargetFPS(60);
 
-    const cube_pos = rl.Vector3.init(screenWidth / 2, screenHeight / 2, 0);
-    const cube_mesh = rl.genMeshCube(200, 200, 200);
-    var cube_model = rl.Model.fromMesh(cube_mesh);
+    var box = Box.init(screenWidth / 2, screenHeight / 2, 0);
 
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
         camera.update(rl.CameraMode.camera_first_person);
 
-        cube_model.transform = cube_model.transform.multiply(rl.Matrix.rotateY(1.0 * 3.14159265358979323846 / 180.0));
-        cube_model.transform = cube_model.transform.multiply(rl.Matrix.rotateZ(1.0 * 3.14159265358979323846 / 180.0));
+        box.rotate();
 
         rl.beginDrawing();
         defer rl.endDrawing();
@@ -38,7 +58,7 @@ pub fn main() anyerror!void {
             camera.begin();
             defer camera.end();
 
-            cube_model.drawEx(cube_pos, Vector3.init(1, 1, 1), 0.0, rl.Vector3.init(1, 1, 1), rl.Color.gold);
+            box.draw();
         }
     }
 }
