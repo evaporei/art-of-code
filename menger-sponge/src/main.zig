9f87231d9d13025220dc37044dac9224e5d90d87
Eva Pace <eba.pachi@gmail.com>
2024-08-21 17:32:03 -0300

    menger-sponge: add camera & cube

--- menger-sponge/src/main.zig
+++ menger-sponge/src/main.zig
@@ -4,18 +4,33 @@ pub fn main() anyerror!void {
     const screenWidth = 800;
     const screenHeight = 450;
 
-    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    rl.initWindow(screenWidth, screenHeight, "menger sponge");
     defer rl.closeWindow(); // Close window and OpenGL context
 
-    rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
+    var camera = rl.Camera3D{
+        .position = rl.Vector3.init(0, 0, 0),
+        .target = rl.Vector3.init(screenWidth / 2, screenHeight / 2, 0),
+        .up = rl.Vector3.init(0, 1, 0),
+        .fovy = 60,
+        .projection = rl.CameraProjection.camera_perspective,
+    };
+
+    rl.disableCursor(); // limit cursor to relative moment inside window
+    rl.setTargetFPS(60);
 
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
+        camera.update(rl.CameraMode.camera_first_person);
 
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.white);
 
-        rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
+        {
+            camera.begin();
+            defer camera.end();
+
+            rl.drawCube(rl.Vector3.init(screenWidth / 2, screenHeight / 2, 0), 200, 200, 200, rl.Color.gold);
+        }
     }
 }
