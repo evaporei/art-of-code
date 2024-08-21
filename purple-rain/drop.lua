9b85fcf23d3ba10d7c064f9221671ab7ca65d58e
Eva Pace <eba.pachi@gmail.com>
2024-08-21 17:32:03 -0300

    purple-rain: add z (a bit slow in the background)

--- purple-rain/drop.lua
+++ purple-rain/drop.lua
@@ -1,26 +1,35 @@
 local Drop = {}
 Drop.__index = Drop
 
-local speed = 200
-
 function Drop.new(x, y)
-    local self = {x = x, y = y}
+    local self = {
+        x = x,
+        y = y,
+    }
+
+    -- 1.0 to 2.0
+    self.z = math.random()
+    -- 100 to 300
+    self.yspeed = 400 * (self.z + math.random() / 2)
+    -- 10 to 20
+    self.length = 10 * (self.z + 1)
 
     setmetatable(self, Drop)
     return self
 end
 
 function Drop:update(dt)
-    self.y = self.y + speed * dt
+    self.y = self.y + self.yspeed * dt
     if self.y > love.graphics.getHeight() then
         self.y = 0
+        self.x = math.random(love.graphics.getWidth())
     end
 end
 
 function Drop:draw()
     love.graphics.setColor(0xEE / 255, 0x82 / 255, 0xEE / 255, 1)
-    -- love.graphics.setLineWidth(3)
-    love.graphics.line(self.x, self.y, self.x, self.y + 10)
+    love.graphics.setLineWidth(self.z * 2)
+    love.graphics.line(self.x, self.y, self.x, self.y + self.length)
 end
 
 return Drop
