b9f9b205b4ec8e8a3e7dd91a4bb77305d38c587e
Eva Pace <eba.pachi@gmail.com>
2024-08-21 17:32:03 -0300

    menger-sponge: first generation (rotation but tho)

--- menger-sponge/src/main.zig
+++ menger-sponge/src/main.zig
@@ -6,13 +6,14 @@ const Model = rl.Model;
 
 const Box = struct {
     pos: Vector3,
+    size: f32,
     model: Model,
 
-    fn init(x: f32, y: f32, z: f32) Box {
+    fn init(x: f32, y: f32, z: f32, size: f32) Box {
         const pos = Vector3.init(x, y, z);
-        const mesh = rl.genMeshCube(200, 200, 200);
+        const mesh = rl.genMeshCube(size, size, size);
         const model = rl.Model.fromMesh(mesh);
-        return Box{ .pos = pos, .model = model };
+        return Box{ .pos = pos, .model = model, .size = size };
     }
 
     fn rotate(self: *Box) void {
@@ -21,7 +22,38 @@ const Box = struct {
     }
 
     fn draw(self: Box) void {
-        self.model.drawEx(self.pos, Vector3.init(1, 1, 1), 0.0, rl.Vector3.init(1, 1, 1), rl.Color.gold);
+        self.model.drawEx(self.pos, Vector3.init(1, 1, 1), 0.0, rl.Vector3.init(1, 1, 1), rl.Color.init(255, 203, 0, 140));
+    }
+
+    fn generate(self: Box, allocator: std.mem.Allocator) anyerror!std.ArrayList(Box) {
+        var next = std.ArrayList(Box).init(allocator);
+
+        // std.debug.print("({},{},{})\n", .{ self.pos.x, self.pos.y, self.pos.z });
+        var x: i8 = -1;
+        while (x < 2) {
+            var y: i8 = -1;
+            while (y < 2) {
+                var z: i8 = -1;
+                while (z < 2) {
+                    const new_size = self.size / 3;
+                    // zig fmt: off
+                    const b = Box.init(
+                        self.pos.x + @as(f32, @floatFromInt(x)) * new_size,
+                        self.pos.y + @as(f32, @floatFromInt(y)) * new_size,
+                        self.pos.z + @as(f32, @floatFromInt(z)) * new_size,
+                        new_size
+                    );
+                    // std.debug.print("({},{},{})\n", .{b.pos.x, b.pos.y, b.pos.z});
+                    // zig fmt: on
+                    try next.append(b);
+                    z += 1;
+                }
+                y += 1;
+            }
+            x += 1;
+        }
+
+        return next;
     }
 };
 
@@ -47,14 +79,23 @@ pub fn main() anyerror!void {
     rl.disableCursor(); // limit cursor to relative moment inside window
     rl.setTargetFPS(60);
 
-    var boxes = std.ArrayList(Box).init(allocator);
-    defer boxes.deinit();
-    try boxes.append(Box.init(0, 0, 0));
+    var sponge = std.ArrayList(Box).init(allocator);
+    try sponge.append(Box.init(0, 0, 0, 200));
 
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
         camera.update(rl.CameraMode.camera_first_person);
 
-        for ((&boxes).items) |*box| {
+        if (rl.isKeyPressed(rl.KeyboardKey.key_j)) {
+            std.debug.print("before: {}\n", .{sponge.items.len});
+            const next = try sponge.items[0].generate(allocator);
+            sponge.deinit();
+            sponge = next;
+            std.debug.print("after: {}\n", .{sponge.items.len});
+        }
+
+        // FIXME: rotation should be for the whole thing
+        // not per box
+        for ((&sponge).items) |*box| {
             box.rotate();
         }
 
@@ -67,9 +108,11 @@ pub fn main() anyerror!void {
             camera.begin();
             defer camera.end();
 
-            for (boxes.items) |box| {
+            for (sponge.items) |box| {
                 box.draw();
             }
         }
     }
+
+    sponge.deinit();
 }
